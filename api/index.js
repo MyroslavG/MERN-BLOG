b1772d828d1a6c392d0646b95c4b85fb0543e0c0
MyroslavG <realmyros@gmail.com>
2023-09-18 12:24:13 -0400

    login + logout + register

--- api/index.js
+++ api/index.js
@@ -2,17 +2,60 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 
-app.use(cors());
+const salt = bcrypt.genSaltSync(10);
+const secret = 'drgfst4wrwgrberg'
+
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://myro:m5Bw8HpcFcaRgqLy@cluster0.x7udsgm.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp');
 
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    const userDoc = await User.create({username, password});
-    res.json(userDoc);
+    try{
+        const userDoc = await User.create({
+            username, 
+            password:bcrypt.hashSync(password,salt),
+        });
+        res.json(userDoc);
+    } catch(e) {
+        res.status(400).json(e);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({username, id: userDoc.id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+                id: userDoc._id,
+                username,
+            });
+        });
+    } else {
+        res.status(400).json('Wrong credentials');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+}); 
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
 })
 
 app.listen(4000);
