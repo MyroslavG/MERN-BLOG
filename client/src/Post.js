14dbcacf029efa85454f1ce25dbb114c52cc0c8c
MyroslavG <realmyros@gmail.com>
2023-09-19 15:02:45 -0400

    posting feature added

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,16 +1,18 @@
-export default function Post() {
+import {formatISO9075} from 'date-fns';
+
+export default function Post({title, summary, content, createdAt, author}) {
     return (
         <div className="post">
             <div className="image">
                 <img src="https://www.imgacademy.com/sites/default/files/23-home-page-golf-club.jpg" alt=""/>
             </div>
             <div className="texts">
-                <h2>Golf club</h2>
+                <h2>{title}</h2>
                 <p className="info">
-                    <span className="author">Myroslav</span>
-                    <time>2023-09-15 10:48</time>
+                    <a className="author">{author && author.username}</a>
+                    <time>{formatISO9075(new Date(createdAt))}</time>
                 </p>
-                <p className="summary">Let's play some golf</p>
+                <p className="summary">{summary}</p>
             </div>
         </div>
     );
